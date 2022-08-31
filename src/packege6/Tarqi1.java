5b2d70b87d8628132d40a0632d4c1e63f4d7ac2d
Tariq Sarwary <Tariq.sarwary100@gmail.com>
2022-08-30 23:12:01 -0400

    second commet

--- src/packege6/Tarqi1.java
+++ src/packege6/Tarqi1.java
@@ -3,5 +3,6 @@ package packege6;
 public class Tarqi1 {
     public static void main(String[] args) {
         System.out.println("Tariq Sarwary");
+        System.out.println("i'm wrintting java code");
     }
 }
