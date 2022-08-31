d0dff5beb4c95ffe10b3b22b7c856daa8f3e671c
Tariq Sarwary <Tariq.sarwary100@gmail.com>
2022-08-30 23:13:02 -0400

    all the last commet

--- src/packege6/Tarqi1.java
+++ src/packege6/Tarqi1.java
@@ -4,5 +4,6 @@ public class Tarqi1 {
     public static void main(String[] args) {
         System.out.println("Tariq Sarwary");
         System.out.println("i'm wrintting java code");
+        System.out.println("Im done for today");
     }
 }
